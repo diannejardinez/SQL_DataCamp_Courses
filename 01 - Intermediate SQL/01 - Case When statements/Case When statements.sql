319efc6799c5616eacf995173dd17640cc7299ca
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-10-26 21:20:22 -0700

    added Q4 from case

--- 01 - Intermediate SQL/01 - Case When statements/Case When statements.sql
+++ 01 - Intermediate SQL/01 - Case When statements/Case When statements.sql
@@ -113,5 +113,50 @@ WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
       AND (awayteam_id = 8633 OR hometeam_id = 8633);
 
 
-      
+
+
+-- Filtering your CASE statement
+-- 05. Filtering your CASE statement
+-- Select team_long_name and team_api_id from team
+SELECT
+	team_long_name,
+	team_api_id
+FROM teams_italy
+-- Filter for team name
+WHERE team_long_name = 'Bologna';
+
+
+-- Select the season and date columns
+SELECT 
+	season,
+	date,
+    -- Identify when Bologna won a match
+	CASE WHEN hometeam_id = 9857 AND home_goal > away_goal THEN 'Bologna Win'
+		WHEN awayteam_id = 9857 AND away_goal > home_goal THEN 'Bologna Win' 
+		END AS outcome
+FROM matches_italy;
+
+-- Select the season, date, home_goal, and away_goal columns
+SELECT 
+	season,
+    date,
+	home_goal,
+	away_goal
+FROM matches_italy
+WHERE 
+-- Exclude games not won by Bologna
+	CASE WHEN hometeam_id = 9857 AND home_goal > away_goal THEN 'Bologna Win'
+		WHEN awayteam_id = 9857 AND away_goal > home_goal THEN 'Bologna Win' 
+		END IS NOT NULL;
+
+
+
+		
+
+
+
+
+
+
+
 
