eb6963c8b8a41a7a7f119bd3cb970e7f4f31fda1
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-10-26 17:43:25 -0700

    adding case sql queries

---
+++ 01 - Intermediate SQL/01 - Case When statements/Case When statements.sql
@@ -0,0 +1,86 @@
+
+
+/*
+Source Code: DataCamp
+Link to course: https://learn.datacamp.com/courses/intermediate-sql
+*/
+
+-- Basic CASE statements
+-- 01. What is your favorite team?
+SELECT
+	-- Select the team long name and team API id
+	team_long_name,
+	team_api_id
+FROM teams_germany
+-- Only include FC Schalke 04 and FC Bayern Munich
+WHERE team_long_name IN ('FC Schalke 04', 'FC Bayern Munich');
+
+
+-- Identify the home team as Bayern Munich, Schalke 04, or neither
+SELECT 
+	CASE WHEN hometeam_id = 10189 THEN 'FC Schalke 04'
+        WHEN hometeam_id = 9823 THEN 'FC Bayern Munich'
+         ELSE 'Other' END AS home_team,
+	COUNT(id) AS total_matches
+FROM matches_germany
+-- Group by the CASE statement alias
+GROUP BY home_team;
+
+
+
+-- CASE statements comparing column values
+-- 02. Creating a list of matches in the 2011/2012 season where Barcelona was the home team
+SELECT 
+	-- Select the date of the match
+	date,
+	-- Identify home wins, losses, or ties
+	CASE WHEN home_goal > away_goal THEN 'Home win!'
+        WHEN home_goal < away_goal THEN 'Home loss :(' 
+        ELSE 'Tie' END AS outcome
+FROM matches_spain;
+
+SELECT 
+	m.date,
+	--Select the team long name column and call it 'opponent'
+	t.team_long_name AS opponent, 
+	-- Complete the CASE statement with an alias
+	CASE WHEN m.home_goal > away_goal THEN 'Home win!'
+        WHEN m.home_goal < away_goal THEN 'Home loss :('
+        ELSE 'Tie' END AS outcome
+FROM matches_spain AS m
+-- Left join teams_spain onto matches_spain
+LEFT JOIN teams_spain AS t
+ON m.awayteam_id = t.team_api_id;
+
+SELECT 
+	m.date,
+	t.team_long_name AS opponent,
+    -- Complete the CASE statement with an alias
+	CASE WHEN m.home_goal > away_goal THEN 'Barcelona win!'
+        WHEN m.home_goal < away_goal THEN 'Barcelona loss :(' 
+        ELSE 'Tie' END AS outcome 
+FROM matches_spain AS m
+LEFT JOIN teams_spain AS t 
+ON m.awayteam_id = t.team_api_id
+-- Filter for Barcelona as the home team
+WHERE m.hometeam_id = 8634; 
+
+ 
+
+
+ -- CASE statements comparing two column values part 2
+ -- 03. Determine the outcome of Barcelona's matches where they played as the away team. 
+-- Select matches where Barcelona was the away team
+SELECT  
+	m.date,
+	t.team_long_name AS opponent,
+	CASE WHEN m.home_goal < away_goal THEN 'Barcelona win!'
+        WHEN m.home_goal > away_goal THEN 'Barcelona loss :(' 
+        ELSE 'Tie' END AS outcome
+FROM matches_spain AS m
+-- Join teams_spain to matches_spain
+LEFT JOIN teams_spain AS t 
+ON m.hometeam_id = t.team_api_id
+WHERE m.awayteam_id = 8634;
+
+
