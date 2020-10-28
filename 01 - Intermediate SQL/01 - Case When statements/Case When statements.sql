fb2320e0a3efa2366be215baa204e58f0a194463
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-10-27 20:36:54 -0700

    adding cout and case when with multiple conditions - sum

--- 01 - Intermediate SQL/01 - Case When statements/Case When statements.sql
+++ 01 - Intermediate SQL/01 - Case When statements/Case When statements.sql
@@ -181,6 +181,17 @@ GROUP BY country;
 
 -- COUNT and CASE WHEN with multiple conditions
 -- 07. Use the country and match table to determine the total number of matches won by the home team in each country during the 2012/2013, 2013/2014, and 2014/2015 seasons.
+SELECT 
+	c.name AS country,
+    -- Sum the total records in each season where the home team won
+	SUM(CASE WHEN m.season = '2012/2013' AND m.home_goal > m.away_goal THEN 1 ELSE 0 END) AS matches_2012_2013,
+ 	SUM(CASE WHEN m.season = '2013/2014' AND m.home_goal > m.away_goal THEN 1 ELSE 0 END) AS matches_2013_2014,
+	SUM(CASE WHEN m.season = '2014/2015' AND m.home_goal > m.away_goal THEN 1 ELSE 0 END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY country;
 
 
 
