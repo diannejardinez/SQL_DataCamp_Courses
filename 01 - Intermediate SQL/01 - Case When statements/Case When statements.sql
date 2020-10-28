a33fd673cd97c8cb66d92f4a1f5d2f47381cdeaa
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-10-27 19:55:20 -0700

    adding count using case when

--- 01 - Intermediate SQL/01 - Case When statements/Case When statements.sql
+++ 01 - Intermediate SQL/01 - Case When statements/Case When statements.sql
@@ -125,7 +125,6 @@ FROM teams_italy
 -- Filter for team name
 WHERE team_long_name = 'Bologna';
 
-
 -- Select the season and date columns
 SELECT 
 	season,
@@ -151,7 +150,48 @@ WHERE
 
 
 
-		
+
+-- COUNT using CASE WHEN
+-- 06. Do the number of soccer matches played in a given European country differ across seasons?
+SELECT 
+	c.name AS country,
+    -- Count games from the 2012/2013 season
+	COUNT(CASE WHEN m.season = '2012/2013'
+				THEN m.id ELSE NULL END) AS matches_2012_2013
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY country;
+
+SELECT 
+	c.name AS country,
+    -- Count matches in each of the 3 seasons
+	COUNT(CASE WHEN m.season = '2012/2013' THEN m.id END) AS matches_2012_2013,
+	COUNT(CASE WHEN m.season = '2013/2014' THEN m.id END) AS matches_2013_2014,
+	COUNT(CASE WHEN m.season = '2014/2015' THEN m.id END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY country;
+
+
+
+
+-- COUNT and CASE WHEN with multiple conditions
+-- 07. Use the country and match table to determine the total number of matches won by the home team in each country during the 2012/2013, 2013/2014, and 2014/2015 seasons.
+
+
+
+
+
+
+
+
+
+
+
 
 
 
