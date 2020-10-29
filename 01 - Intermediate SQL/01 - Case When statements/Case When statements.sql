a0056865cb20c5cedca1e12b2510fb8098bee6a1
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-10-28 23:46:23 -0700

    adding calculating percent with case and avg

--- 01 - Intermediate SQL/01 - Case When statements/Case When statements.sql
+++ 01 - Intermediate SQL/01 - Case When statements/Case When statements.sql
@@ -195,6 +195,33 @@ GROUP BY country;
 
 
 
+-- Calculating percent with CASE and AVG
+-- 08. Examine the number of wins, losses, and ties in each country. The matches table is filtered to include all matches from the 2013/2014 and 2014/2015 seasons.
+SELECT 
+    c.name AS country,
+    -- Count the home wins, away wins, and ties in each country
+	COUNT(CASE WHEN m.home_goal > m.away_goal THEN m.id END) AS home_wins,
+	COUNT(CASE WHEN m.home_goal < m.away_goal THEN m.id END) AS away_wins,
+	COUNT(CASE WHEN m.home_goal = m.away_goal THEN m.id END) AS ties
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
+
+SELECT 
+	c.name AS country,
+    -- Calculate the percentage of tied games in each season
+	AVG(CASE WHEN m.season= '2013/2014' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season= '2013/2014' AND m.home_goal != m.away_goal THEN 0
+			 END) AS ties_2013_2014,
+	AVG(CASE WHEN m.season= '2014/2015' AND m.home_goal = m.away_goal THEN 1
+			 WHEN m.season= '2014/2015' AND m.home_goal != m.away_goal THEN 0
+			 END) AS ties_2014_2015
+FROM country AS c
+LEFT JOIN matches AS m
+ON c.id = m.country_id
+GROUP BY country;
+
 
 
 
