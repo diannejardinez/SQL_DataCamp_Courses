2bcb473eade0c63b04468eb20f128d239ada88a2
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-11-02 17:28:06 -0800

    adding Filtering using a subquery with a list

--- 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
+++ 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
@@ -15,3 +15,9 @@ FROM  matches_2013_2014
 WHERE (home_goal + away_goal) > 
        (SELECT 3 * AVG(home_goal + away_goal)
         FROM matches_2013_2014); 
+
+
+
+
+ -- Filtering using a subquery with a list
+ -- 02. generate a list of teams that never played a game in their home city.
