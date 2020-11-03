ea606897629494566bd5e40073b51cc4e6cdbda3
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-11-02 17:32:27 -0800

    Filtering with more complex subquery conditions

--- 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
+++ 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
@@ -21,3 +21,25 @@ WHERE (home_goal + away_goal) >
 
  -- Filtering using a subquery with a list
  -- 02. generate a list of teams that never played a game in their home city.
+ SELECT 
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team 
+-- Exclude all values from the subquery
+WHERE team_api_id NOT IN 
+     (SELECT DISTINCT hometeam_ID  FROM match);
+
+
+-- Filtering with more complex subquery conditions
+-- 03. creating a list of teams that scored 8 or more goals in a home match.
+SELECT
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team
+-- Filter for teams with 8 or more home goals
+WHERE team_api_id IN
+	  (SELECT hometeam_ID 
+       FROM match
+       WHERE home_goal >= 8);
