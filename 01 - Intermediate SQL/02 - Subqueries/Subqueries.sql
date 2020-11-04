872a14a6bb3bd678d8ce09eeb0e9472f11d953d0
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-11-03 17:11:39 -0800

    Building on Subqueries in FROM

--- 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
+++ 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
@@ -76,6 +76,31 @@ GROUP BY country_name;
 
 
 
+-- Building on Subqueries in FROM
+-- 05. Find out some more details about those matches -- when they were played, during which seasons, and how many of the goals were home vs. away goals
+SELECT
+	-- Select country, date, home, and away goals from the subquery
+    country,
+    date,
+    home_goal,
+    away_goal
+FROM 
+	-- Select country name, date, and total goals in the subquery
+	(SELECT c.name AS country, 
+     	    m.date, 
+     		m.home_goal, 
+     		m.away_goal,
+           (m.home_goal + m.away_goal) AS total_goals
+    FROM match AS m
+    LEFT JOIN country AS c
+    ON m.country_id = c.id) AS subq
+-- Filter by total goals scored in the main query
+WHERE total_goals >= 10;
+
+
+
+
+
 
 
 
