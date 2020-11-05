78f9acc22ea5fe0353827c9c0d39e450146ec22c
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-11-04 19:46:34 -0800

    Add a subquery to the SELECT clause

--- 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
+++ 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
@@ -100,7 +100,22 @@ WHERE total_goals >= 10;
 
 
 
-
+-- Add a subquery to the SELECT clause
+-- 06. Construct a query that calculates the average number of goals per match in each country's league.
+SELECT 
+	l.name AS league,
+    -- Select and round the league's total goals
+    ROUND(AVG(m.home_goal + m.away_goal), 2) AS avg_goals,
+    -- Select & round the average total goals for the season
+    (SELECT ROUND(AVG(home_goal + away_goal), 2) 
+     FROM match
+     WHERE season = '2013/2014') AS overall_avg
+FROM league AS l
+LEFT JOIN match AS m
+ON l.country_id = m.country_id
+-- Filter for the 2013/2014 season
+WHERE m.season = '2013/2014'
+GROUP BY league;
 
 
 
