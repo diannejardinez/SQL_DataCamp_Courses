dda1857509f36225225a81d6ddb6209b910e2f11
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-11-05 22:31:48 -0800

    Subqueries in Select for Calculations

--- 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
+++ 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
@@ -120,4 +120,25 @@ GROUP BY league;
 
 
 
+-- Subqueries in Select for Calculations
+-- 07. In the previous exercise, you created a column to compare each league's average total goals to the overall average goals in the 2013/2014 season. In this exercise, you will add a column that directly compares these values by subtracting the overall average from the subquery.
+SELECT
+	-- Select the league name and average goals scored
+	l.name AS league,
+	ROUND(AVG(m.home_goal + m.away_goal),2) AS avg_goals,
+    -- Subtract the overall average from the league average
+	ROUND(AVG(m.home_goal + m.away_goal)  -
+		(SELECT AVG(home_goal + away_goal)
+		 FROM match 
+         WHERE season = '2013/2014'),2) AS diff
+FROM league AS l
+LEFT JOIN match AS m
+ON l.country_id = m.country_id
+-- Only include 2013/2014 results
+WHERE m.season = '2013/2014'
+GROUP BY l.name;
+
+
+
+
 
