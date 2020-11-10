d3a3c3d393df0c16591d1bb27fc30b3d2fdab115
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-11-09 21:23:22 -0800

    Add a subquery in FROM

--- 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
+++ 01 - Intermediate SQL/02 - Subqueries/Subqueries.sql
@@ -161,4 +161,26 @@ GROUP BY m.stage;
 
 
 
+-- Add a subquery in FROM
+-- 09. turn the main query into a subquery to extract a list of stages where the average home goals in a stage is higher than the overall average for home goals in a match.
+SELECT 
+	-- Select the stage and average goals from the subquery
+	s.stage,
+	ROUND(s.avg_goals,2) AS avg_goals
+FROM 
+	-- Select the stage and average goals in 2012/2013
+	(SELECT
+		 stage,
+         AVG(home_goal + away_goal) AS avg_goals
+	 FROM match
+	 WHERE season = '2012/2013'
+	 GROUP BY stage) AS s
+WHERE 
+	-- Filter the main query using the subquery
+	s.avg_goals > (SELECT AVG(home_goal + away_goal) 
+                    FROM match WHERE season = '2012/2013');
+
+
+
+
 
