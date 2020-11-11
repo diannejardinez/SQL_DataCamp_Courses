50f93a375c2cd7c7d8e244b04c11a5493b852dd4
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-11-10 19:41:06 -0800

    Correlated subquery with multiple conditions

--- 01 - Intermediate SQL/03 - Correlated:Nested Queries and Table Expressions /correlated_subqueries.sql
+++ 01 - Intermediate SQL/03 - Correlated:Nested Queries and Table Expressions /correlated_subqueries.sql
@@ -14,3 +14,21 @@ WHERE
          FROM match AS sub
          -- Join the main query to the subquery in WHERE
          WHERE main.country_id = sub.country_id);
+
+
+-- Correlated subquery with multiple conditions
+-- 02. What was the highest scoring match for each country, in each season?
+SELECT 
+	-- Select country ID, date, home, and away goals from match
+	main.country_id,
+    main.date,
+    main.home_goal,
+    main.away_goal
+FROM match AS main
+WHERE 
+	-- Filter for matches with the highest number of goals scored
+	(home_goal + away_goal) = 
+        (SELECT MAX(sub.home_goal + sub.away_goal)
+         FROM match AS sub
+         WHERE main.country_id = sub.country_id
+               AND main.season = sub.season);
