e1f72ab318bc0bc487ed8dc40e058f5f20de9e03
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-11-10 19:25:41 -0800

    Basic Correlated Subqueries

--- 01 - Intermediate SQL/03 - Correlated:Nested Queries and Table Expressions /correlated_subqueries.sql
+++ 01 - Intermediate SQL/03 - Correlated:Nested Queries and Table Expressions /correlated_subqueries.sql
@@ -1,2 +1,16 @@
 -- Basic Correlated Subqueries
 -- 01. examine matches with scores that are extreme outliers for each country -- above 3 times the average score
+SELECT 
+	-- Select country ID, date, home, and away goals from match
+	main.country_id,
+    date,
+    main.home_goal, 
+    away_goal
+FROM match AS main
+WHERE 
+	-- Filter the main query by the subquery
+	(home_goal + away_goal) > 
+        (SELECT AVG((sub.home_goal + sub.away_goal) * 3)
+         FROM match AS sub
+         -- Join the main query to the subquery in WHERE
+         WHERE main.country_id = sub.country_id);
