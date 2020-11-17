ab1fb8c61ffcd06d70b4c647a22f25579688050d
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-11-16 19:27:22 -0800

    Clean up with CTEs

--- 01 - Intermediate SQL/03 - Correlated:Nested Queries and Table Expressions /correlated_subqueries.sql
+++ 01 - Intermediate SQL/03 - Correlated:Nested Queries and Table Expressions /correlated_subqueries.sql
@@ -102,3 +102,32 @@ LEFT JOIN (
   GROUP BY country_id, season) AS outer_s
 ON c.id = outer_s.country_id
 GROUP BY country;
+
+
+-- Common Table Expressions(CTE)
+-- Clean up with CTEs
+-- 05. Using CTE, generate a list of countries and the number of matches in each country with more than 10 total goals.
+-- Set up your CTE
+WITH match_list AS (
+    SELECT 
+      country_id, 
+      id
+    FROM match
+    WHERE (home_goal + away_goal) >= 10)
+-- Select league and count of matches from the CTE
+SELECT
+    l.name AS league,
+    COUNT(match_list.id) AS matches
+FROM league AS l
+-- Join the CTE to the league table
+LEFT JOIN match_list ON l.id = match_list.country_id
+GROUP BY l.name;
+
+
+
+
+
+
+
+
+
